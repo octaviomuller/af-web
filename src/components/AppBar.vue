e36f266eca9e0ebd86ab3b715c5c119fe3c42f4f
octaviohmb <octaviohmb@gmail.com>
2021-11-19 20:59:24 -0300

    app bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar
+    absolute
+    color="white"
+  >
+    <v-toolbar-title>My Vue App</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
