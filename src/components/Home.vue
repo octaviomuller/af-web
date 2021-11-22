306864a95527eb3dc21338e0de99b2803a48617f
octaviohmb <octaviohmb@gmail.com>
2021-11-21 22:52:22 -0300

    fixing main content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <div style="height: 60vh" class="d-flex justify-center align-center">
-      <v-img
+  <v-content>
+    <v-img
         contain
         max-width="800"
         class="align-center justify-center"
         src="../assets/home_content.png"
       ></v-img>
-    </div>
-  </div>
+  </v-content>
 </template>
